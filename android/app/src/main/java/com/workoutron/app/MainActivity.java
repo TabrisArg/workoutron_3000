20c0d8776fe1e9d582099d4d37138378f9960c02
Cesar <c.gozurreta@dsfishlabs.com>
2026-02-02 09:49:37 +0100

    first commit

---
+++ android/app/src/main/java/com/workoutron/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.workoutron.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.workoutron.app',
+  appName: 'Workoutron3000',
+  webDir: 'dist'
+};
+
+export default config;
